a8bf48260e9317d6f911df578c3f51b5ad26800c
thaleswillreis <thaleswill@yahoo.com.br>
2023-02-21 20:58:17 -0300

    Extraindo a 'latitude' e a 'longitude' da API Geoplugin

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,7 +1,14 @@
 import requests
+import json
+import pprint
 
 reqst_coordinates = requests.get('http://www.geoplugin.net/json.gp')
+
 if (reqst_coordinates.status_code != 200):
     print('Não foi possível obter a localização')
 else:
-    print(reqst_coordinates.text)
+    localizacao = json.loads(reqst_coordinates.text)
+    latitude = localizacao['geoplugin_latitude']
+    longitude = localizacao['geoplugin_longitude']
+    print('Latitude: ', latitude)
+    print('Longitude: ', longitude)
