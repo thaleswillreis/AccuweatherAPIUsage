c1da6428df2d6dc6a1b6aa516b1152d098620670
thaleswillreis <thaleswill@yahoo.com.br>
2023-02-21 22:45:47 -0300

    Extraindo a 'Localidade' e o 'Código do Local' da API Accuweather

--- weather_forecast.py
+++ weather_forecast.py
@@ -2,13 +2,31 @@ import requests
 import json
 import pprint
 
+accuweatherAPIKey = '	IGB5xYr0Y6YXQfmBA2CROn5dQMcF105T'
+
 reqst_coordinates = requests.get('http://www.geoplugin.net/json.gp')
 
 if (reqst_coordinates.status_code != 200):
-    print('Não foi possível obter a localização')
+    print('Não foi possível obter as coordenadas')
 else:
     localizacao = json.loads(reqst_coordinates.text)
     latitude = localizacao['geoplugin_latitude']
     longitude = localizacao['geoplugin_longitude']
-    print('Latitude: ', latitude)
-    print('Longitude: ', longitude)
+    # print('Latitude: ', latitude)
+    # print('Longitude: ', longitude)
+
+    locationAPIUrl = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + \
+        accuweatherAPIKey + "&q=" + latitude + "%2C%20" + longitude + "&language=pt-br"
+
+    reqst_location = requests.get(locationAPIUrl)
+    if (reqst_location.status_code != 200):
+        print('Não foi possível obter o código do local')
+    else:
+        locationResponse = json.loads(reqst_location.text)
+        nomeLocal = locationResponse['LocalizedName'] + ', ' + \
+            locationResponse['AdministrativeArea']['LocalizedName'] + \
+            '. ' + locationResponse['Country']['LocalizedName']
+        codigoLocal = locationResponse['Key']
+        # print(pprint.pprint(json.loads(reqst_location.text)))
+        print('Local: ', nomeLocal)
+        print('Código do Local: ', codigoLocal)
