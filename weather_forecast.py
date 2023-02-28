d2e16813d48f81061a4e8cdc8b17b8fba196571d
thaleswillreis <thaleswill@yahoo.com.br>
2023-02-27 22:26:37 -0300

    Code compliance with the Python style guide - PEP 8

--- weather_forecast.py
+++ weather_forecast.py
@@ -3,11 +3,15 @@ import json
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
-accuweatherAPIKey = (os.environ["accuweatherAPIKey"])
+accuweather_api_key = (os.environ["accuweatherAPIKey"])
+"""Environment variable containing the accuWeather API key.
+    Can be replaced by the API Key String directly.
+    """
 
 
-def getCoordinates():
+def get_coordinates():
     """Get the geolocation of the host from the Geoplugin API
 
     Returns:
@@ -16,16 +20,20 @@ def getCoordinates():
     reqst_coordinates = requests.get('http://www.geoplugin.net/json.gp')
 
     if (reqst_coordinates.status_code != 200):
-        print('Não foi possível obter as coordenadas')
+        print('ERRO: Não foi possível obter as coordenadas da sua localização.')
+        return None
     else:
-        location = json.loads(reqst_coordinates.text)
-        coordinates = {}
-        coordinates['latitude'] = location['geoplugin_latitude']
-        coordinates['longitude'] = location['geoplugin_longitude']
-        return coordinates
+        try:
+            location = json.loads(reqst_coordinates.text)
+            coordinates = {}
+            coordinates['latitude'] = location['geoplugin_latitude']
+            coordinates['longitude'] = location['geoplugin_longitude']
+            return coordinates
+        except:
+            return None
 
 
-def getLocationCode(latitude, longitude):
+def get_location_code(latitude, longitude):
     """Get the location code and host location information from the Accuweather API.
 
     Args:
@@ -33,25 +41,31 @@ def getLocationCode(latitude, longitude):
         longitude (__str__): Host longitude
 
     Returns:
-        __dict__: Returns a dictionary containing the Location Code and Location data (Location Name, Country, State, Province, etc).
+        __dict__: Returns a dictionary containing the Location Code and Location data 
+        (Location Name, Country, State, Province, etc).
     """
-    locationAPIUrl = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + \
-        accuweatherAPIKey + "&q=" + latitude + "%2C%20" + longitude + "&language=pt-br"
+    location_api_url = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + \
+        accuweather_api_key + "&q=" + latitude + \
+        "%2C%20" + longitude + "&language=pt-br"
 
-    reqst_location = requests.get(locationAPIUrl)
+    reqst_location = requests.get(location_api_url)
     if (reqst_location.status_code != 200):
-        print('Não foi possível obter o código do local')
+        print('ERRO: Não foi possível obter o código do local.')
+        return None
     else:
-        locationResponse = json.loads(reqst_location.text)
-        locationInfo = {}
-        locationInfo['localName'] = locationResponse['LocalizedName'] + ', ' + \
-            locationResponse['AdministrativeArea']['LocalizedName'] + \
-            '. ' + locationResponse['Country']['LocalizedName']
-        locationInfo['localCode'] = locationResponse['Key']
-        return locationInfo
+        try:
+            location_response = json.loads(reqst_location.text)
+            location_info = {}
+            location_info['localName'] = location_response['LocalizedName'] + ', ' + \
+                location_response['AdministrativeArea']['LocalizedName'] + \
+                '. ' + location_response['Country']['LocalizedName']
+            location_info['localCode'] = location_response['Key']
+            return location_info
+        except:
+            return None
 
 
-def getWeatherForecast(localCode, localName):
+def get_weather_forecast(localCode, localName):
     """Get current weather conditions for a location from the Accuweather API.
 
     Args:
@@ -61,25 +75,35 @@ def getWeatherForecast(localCode, localName):
     Returns:
         __dict__: Returns a dictionary containing weather conditions for the location.
     """
-    currentConditionsAPIUrl = "http://dataservice.accuweather.com/currentconditions/v1/" + \
-        localCode + "?apikey=" + accuweatherAPIKey + "&language=pt-br"
+    current_conditions_api_url = "http://dataservice.accuweather.com/currentconditions/v1/" + \
+        localCode + "?apikey=" + accuweather_api_key + "&language=pt-br"
 
-    reqst_weather_conditions = requests.get(currentConditionsAPIUrl)
+    reqst_weather_conditions = requests.get(current_conditions_api_url)
     if (reqst_weather_conditions.status_code != 200):
-        print('Não foi possível obter as condições de tempo do local')
+        print('ERRO: Não foi possível obter as condições de tempo do local.')
+        return None
     else:
-        currentConditionsResponse = json.loads(reqst_weather_conditions.text)
-        weatherConditions = {}
-        weatherConditions['weatherText'] = currentConditionsResponse[0]['WeatherText']
-        weatherConditions['temperature'] = currentConditionsResponse[0]['Temperature']['Metric']['Value']
-        weatherConditions['localName'] = localName
-        return weatherConditions
+        try:
+            current_conditions_response = json.loads(
+                reqst_weather_conditions.text)
+            weather_conditions = {}
+            weather_conditions['weatherText'] = current_conditions_response[0]['WeatherText']
+            weather_conditions['temperature'] = current_conditions_response[0]['Temperature']['Metric']['Value']
+            weather_conditions['localName'] = localName
+            return weather_conditions
+        except:
+            return None
 
 
-coordinates = getCoordinates()
-local = getLocationCode(coordinates['latitude'], coordinates['longitude'])
-currentWeather = getWeatherForecast(local['localCode'], local['localName'])
+coordinates = get_coordinates()
 
-print('O clima neste momento em ' + currentWeather['localName'])
-print(currentWeather['weatherText'])
-print('Temperatura: ' + str(currentWeather['temperature']) + "\xb0" + "C")
+try:
+    local = get_location_code(
+        coordinates['latitude'], coordinates['longitude'])
+    current_weather = get_weather_forecast(
+        local['localCode'], local['localName'])
+    print('O clima neste momento em ' + current_weather['localName'])
+    print(current_weather['weatherText'])
+    print('Temperatura: ' + str(current_weather['temperature']) + "\xb0" + "C")
+except:
+    print('ATENÇÃO: Ocorreu um erro ao processar sua solicitação. Contacte o suporte.')
